a31e39931c3c347012c5dfbba0e35f793b151f3e
Maverick Grayer <maverick72032@gmail.com>
2023-11-16 12:38:12 -0500

    Assignment Complete

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double temp = 80;
+            Console.WriteLine($"The current temp is {temp} Fahrenheit.");
+            Console.WriteLine($"But if I invoke my all powerful method upon it...");
+            temp = TempConverter.FahrenheitToCelsius(temp);
+            Console.WriteLine($"the temp is now {temp} Celsius!");
+            Console.WriteLine("And I can change it right back. Kapow!");
+            temp = TempConverter.CelsiusToFahrenheit(temp);
+            Console.WriteLine($"The temp is once again {temp} Fahrenheit.");
         }
     }
 }
